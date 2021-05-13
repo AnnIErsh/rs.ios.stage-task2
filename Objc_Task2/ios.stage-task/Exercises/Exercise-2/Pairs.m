bb56b5d29af43ae30d7086bf385253836d90297a
Anna <annaersh1994@gmail.com>
2021-05-14 01:43:25 +0300

    feat: add exercise2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -2,8 +2,24 @@
 
 @implementation Pairs
 
-- (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+- (NSInteger)makeDifference:(NSMutableArray <NSNumber *> *)array withNumber:(NSInteger)n andCounter:(NSUInteger)count{
+    size_t size = array.count - 1;
+    if (size)
+    {
+        for (int i = 0; i < size + 1; i++)
+        {
+            if ([array[size] intValue] - [array[size - i] intValue] == n)
+                count++;
+        }
+        [array removeLastObject];
+        return [self makeDifference:array withNumber:n andCounter:count];
+    }
+    return count;
 }
 
+- (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
+    NSArray *arr = [array sortedArrayUsingSelector:@selector(compare:)];
+    NSUInteger pairsCount = [self makeDifference:arr.mutableCopy withNumber:number.intValue andCounter:0];
+    return pairsCount;
+}
 @end

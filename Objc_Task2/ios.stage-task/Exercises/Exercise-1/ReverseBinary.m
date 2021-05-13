f4cf2e3d0b600befbfc84b3ef675c8eb1d59f649
Anna <annaersh1994@gmail.com>
2021-05-13 23:33:54 +0300

    init: start with exercise 1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,12 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 bit = 0;
+    UInt8 res = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        bit = (n >> i) & 1;
+        res = (res << 1) | bit;
+    }
+    return res;
 }

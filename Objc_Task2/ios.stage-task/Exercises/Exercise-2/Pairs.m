75230b5d3f60dd48aace40610133328d59e69d05
Anna <annaersh1994@gmail.com>
2021-05-14 18:39:07 +0300

    fix: remove recursion from ex2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -2,24 +2,26 @@
 
 @implementation Pairs
 
-- (NSInteger)makeDifference:(NSMutableArray <NSNumber *> *)array withNumber:(NSInteger)n andCounter:(NSUInteger)count{
-    size_t size = array.count - 1;
-    if (size)
+- (NSInteger)makeDifference:(NSArray <NSNumber *> *)array withNumber:(NSInteger)numb andSize:(NSInteger)size atPosition:(int)index {
+    NSInteger count = 0;
+    NSInteger diff = 0;
+    for (int i = index + 1; i < size; i++)
     {
-        for (int i = 0; i < size + 1; i++)
-        {
-            if ([array[size] intValue] - [array[size - i] intValue] == ABS(n))
-                count++;
-        }
-        [array removeLastObject];
-        return [self makeDifference:array withNumber:n andCounter:count];
+        diff = [array[index] intValue] - [array[i] intValue];
+        (diff == numb) || (diff * (-1) == numb) ? count++ : count;
     }
     return count;
 }
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
+    NSInteger size = array.count;
+    NSInteger pairCount = 0;
+    if (!array || [array isEqualToArray:@[]] || size < 2)
+        return 0;
     NSArray *arr = [array sortedArrayUsingSelector:@selector(compare:)];
-    NSUInteger pairsCount = [self makeDifference:arr.mutableCopy withNumber:number.intValue andCounter:0];
-    return pairsCount;
+    for (int i = 0; i < size; i++)
+        pairCount += [self makeDifference:arr withNumber:number.intValue andSize:size atPosition:i];
+    return pairCount;
 }
+
 @end
